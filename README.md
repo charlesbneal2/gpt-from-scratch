66de658e3f41aeb282f2bca7b3826648896371fd
Charles <charlesbneal2@gmail.com>
2025-01-13 21:35:20 -0600

    readme, basic character tokenizer

---
+++ README.md
@@ -0,0 +1,2 @@
+# GPT FROM SCRATCH
+Example GPT implementation following Andrej Karpathy's tutorial: https://www.youtube.com/watch?v=kCc8FmEb1nY
---
+++ tokenizer.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class CharacterTokenizer:
+    def __init__(self):
+        self.encoder = dict()
+        self.decoder = dict()
+
+    def fit(self, text: str):
+        chars = set(text)
+        for i, c in enumerate(chars):
+            self.encoder[c] = i
+            self.decoder[i] = c
+
+    def encode(self, text: str):
+        return [self.encoder[c] for c in text]
+
+    def decode(self, vector: List[int]):
+        return "".join([self.decoder[i] for i in vector])
